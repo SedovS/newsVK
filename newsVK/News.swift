bb137dd5bae30436ac8e93a85c6ac58c44b9aa76
SedovS <Neusedelka@yandex.ru>
2019-11-10 23:05:29 +0300

    remove field photo

--- newsVK/News.swift
+++ newsVK/News.swift
@@ -11,13 +11,12 @@ import UIKit
 class News {
     public let text: String
     public let urlPhoto: String
-    public var photo: UIImage?
     
     init(text: String, urlPhoto: String){
         self.text = text
         self.urlPhoto = urlPhoto
-        self.photo = nil
     }
 }
 
 var globalArrayNews = [News]()
+var globaldictionaryCasheImege = NSCache<NSString, UIImage>()
