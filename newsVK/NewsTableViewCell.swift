f57c296415cd093d9458fed6d8094cb203ce27b1
SedovS <Neusedelka@yandex.ru>
2019-11-10 15:20:38 +0300

    init NewsTable

---
+++ newsVK/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  newsVK
+//
+//  Created by Apple on 10.11.2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
