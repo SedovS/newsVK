dbbf1e37a80afceeb8a0686e4a25e102f096bd4f
SedovS <Neusedelka@yandex.ru>
2019-11-10 10:50:42 +0300

    add authorization VC

---
+++ newsVK/AuthorizationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AuthorizationViewController.swift
+//  newsVK
+//
+//  Created by Apple on 10.11.2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AuthorizationViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
