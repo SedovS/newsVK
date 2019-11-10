d0d94715ddbdf02723e522d55947e82b61eb809c
SedovS <Neusedelka@yandex.ru>
2019-11-11 01:38:24 +0300

    realize func dynamicView

--- newsVK/DetailsViewController.swift
+++ newsVK/DetailsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class DetailsViewController: UIViewController {
 
@@ -17,23 +18,46 @@ class DetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        dynamicView()
        
-        // Do any additional setup after loading the view.
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-         textLabel.text = news.text
+    private func dynamicView() -> Void {
+        let scrolView = UIScrollView()
+        let widthScrolView = view.frame.width
+        let heightScrolView = view.frame.height
+        let contentView = UIView()
+        let wightPhoto = news.width
+        let heightPhoto = news.height
+      
+        let proportion = Double(wightPhoto) / Double(heightPhoto)
+        let width = Double(widthScrolView - 20)
+        var height = Double(width / proportion)
+      
+        imageView.frame = CGRect(x: 10, y: 10, width: width, height: height)
         imageView.image = globaldictionaryCasheImege.object(forKey: news!.urlPhoto as NSString)
+        height += 30
+        
+        let text = news.text
+        textLabel.frame = CGRect(x: 20, y: height, width: width - 20, height: 10)
+        textLabel.numberOfLines = 0
+        textLabel.font = .systemFont(ofSize: CGFloat(18))
+        textLabel.textAlignment = .center
+        textLabel.text = text
+        textLabel.sizeToFit()
+    
+        height += Double(textLabel.frame.height)
+        height += 50
+        
+        contentView.addSubview(imageView)
+        contentView.addSubview(textLabel)
+        
+        scrolView.frame = CGRect(x: 0, y: 0, width: Double(widthScrolView), height: Double(heightScrolView))
+        contentView.frame = CGRect(x: 0, y: 0, width: Double(widthScrolView), height: height)
+        
+        scrolView.contentSize.height = contentView.frame.height
+        scrolView.addSubview(contentView)
+        view.addSubview(scrolView)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
