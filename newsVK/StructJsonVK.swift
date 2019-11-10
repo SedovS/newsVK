9d8650057cb8be010f9a00b6e3c4e55a7409291b
SedovS <Neusedelka@yandex.ru>
2019-11-10 18:05:58 +0300

    add structure Fson from VK for decode

---
+++ newsVK/StructJsonVK.swift
@@ -0,0 +1,39 @@
+//
+//  Model.swift
+//  newsVK
+//
+//  Created by Apple on 10.11.2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+
+struct StructJsonVK: Codable  {
+    let response: Response
+}
+
+struct Response: Codable{
+    let items : [Items]
+    let nextFrom: String
+}
+
+struct Items: Codable {
+    let text: String? //Есть посты без текста
+    let attachments: [Attachments]? //Есть посты без Attachments (например репосты)
+}
+
+struct Attachments: Codable {
+    let type: String
+    let photo: Photo
+}
+
+struct Photo: Codable {
+    let sizes: [Sizes]
+}
+
+struct Sizes: Codable {
+    let type: String
+    let url: String
+    let wight: Int
+    let height: Int
+}
